35b7dbd2c01891a3e7224ad007bde8afdaf9ec77
edenlisk <nkumbuyenock@gmail.com>
2023-03-14 10:22:49 +0200

    setup classes

---
+++ student.rb
@@ -0,0 +1,14 @@
+require 'person'
+
+class Student < Person
+  attr_accessor :class_room
+
+  def initialize(age, class_room, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @class_room = class_room
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
